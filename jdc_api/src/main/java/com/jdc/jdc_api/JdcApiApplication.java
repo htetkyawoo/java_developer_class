8b3622b5bbcaf8a57cb93cf67a2c52ac30c9e5ea
Htet Kyaw Oo <htetkyawu@gmail.com>
2024-06-05 11:07:50 +0630

    start java_developer_class backend

---
+++ jdc_api/src/main/java/com/jdc/jdc_api/JdcApiApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.jdc_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdcApiApplication.class, args);
+	}
+
+}
